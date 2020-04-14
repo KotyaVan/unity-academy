64c0c64d7a4bb8e0dc898b7a762f36004e902971
Vanya Malykhin <gamesforlife.gfl@gmail.com>
2020-04-14 14:48:16 +0300

    Figures falling -> Up figure - is next

---
+++ Assets/Scripts/FallingFigure.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingFigure : MonoBehaviour
+{
+
+	[SerializeField] private NextFigure _nextFigure;
+	private GameObject _figure;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (_figure)
+		{
+			_figure.transform.Translate(- Vector3.up * Time.deltaTime);
+
+			if (_figure.transform.position.y < -3)
+			{
+			    Destroy(_figure);
+			}
+			
+		}
+		else
+		{
+			_figure = Instantiate(_nextFigure.GetFigureForFalling());
+			_nextFigure.UpdateNextFigure();
+		}
+
+	}
+}
---
+++ Assets/Scripts/NextFigure.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+
+public class NextFigure : MonoBehaviour
+{
+    private Random _random = new Random();
+    private GameObject _figure;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        GenerateFigure();
+    }
+
+    private void GenerateFigure()
+    {
+        int figureName = _random.Next(4);
+
+        Debug.Log("Random " + figureName);
+
+        switch (figureName)
+        {
+            case 0:
+                _figure = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                break;
+            case 1:
+                _figure = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                break;
+            case 2:
+                _figure = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                break;
+            case 3:
+                _figure = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                break;
+            case 4:
+                _figure = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                break;
+                
+        }
+    }
+
+    public GameObject GetFigureForFalling()
+    {
+        return _figure;
+    }
+
+    public void UpdateNextFigure()
+    {
+        Destroy(_figure);
+        GenerateFigure();
+    }
+}
