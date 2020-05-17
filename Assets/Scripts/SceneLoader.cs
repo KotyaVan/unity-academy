ffa5c10950ca4609c38098739e79bc6e3dfdb4d2
Vanya Malykhin <gamesforlife.gfl@gmail.com>
2020-05-17 23:01:13 +0300

    Home task, loader and fade out

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,9 +5,29 @@ using UnityEngine.SceneManagement;
 
 namespace DefaultNamespace
 {
+    
     public class SceneLoader : MonoBehaviour
     {
+        [SerializeField] private GameObject preloaderImage;
         private static string nextLevel;
+        public Texture2D fadeTexture;
+ 
+        [Range(0.1f,1f)]
+        public float fadespeed;
+        public int drawDepth = -1000;
+ 
+        private float alpha = 1f;
+        private float fadeDir = -1f;
+
+        private void Update()
+        {
+            if (preloaderImage)
+            {
+                var rotation = preloaderImage.transform.rotation;
+                rotation = new Quaternion(0, 0, rotation.z - 0.01f, rotation.w);
+                preloaderImage.transform.rotation = rotation;
+            }
+        }
 
         public static void LoadLevel(string level)
         {
@@ -38,6 +58,23 @@ namespace DefaultNamespace
 
             nextLevel = null;
             SceneManager.UnloadSceneAsync("LoadingScene");
+
+        }
+
+        private void OnGUI()
+        {
+            
+            alpha += fadeDir * fadespeed * Time.deltaTime;
+            alpha = Mathf.Clamp01(alpha);
+ 
+            Color newColor = GUI.color; 
+            newColor.a = alpha;
+ 
+            GUI.color = newColor;
+ 
+            GUI.depth = drawDepth;
+ 
+            GUI.DrawTexture( new Rect(0,0, Screen.width, Screen.height), fadeTexture);
 
         }
     }
