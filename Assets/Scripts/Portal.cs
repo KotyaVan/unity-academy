aa6750e5c5a7f7199a629e8d76a86fdf58de6fc5
Vanya Malykhin <gamesforlife.gfl@gmail.com>
2020-06-04 19:22:07 +0300

    last lesson

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -11,7 +11,7 @@ namespace DefaultNamespace
 
         private Player player;
         
-        private void OnTriggerEnter2D(Collider other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
             player = other.transform.GetComponent<Player>();
             
