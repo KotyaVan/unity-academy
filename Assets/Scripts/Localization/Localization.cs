31848da6a58750e57a74a53ce5ed6978544abafd
Vanya Malykhin <gamesforlife.gfl@gmail.com>
2020-08-20 20:23:08 +0300

    Language Utils

---
+++ Assets/Scripts/Localization/Localization.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Localization : MonoBehaviour
+{
+    private Text text;
+    private string key;
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+        key = text.text;
+        LocalizationController.ChangeLocalization += ChangeLocalization;
+    }
+
+    private void OnDestroy()
+    {
+        LocalizationController.ChangeLocalization -= ChangeLocalization;
+    }
+
+    private void ChangeLocalization(string loc)
+    {
+        text.text = LocalizationController.LocalizedText[key];
+    }
+}
---
+++ Assets/Scripts/Localization/LocalizationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+public class LocalizationController : MonoBehaviour
+{
+    public static Dictionary<string, string> LocalizedText;
+    public static Action<string> ChangeLocalization = (loc) =>
+    {
+        LoadLocalizationFile(loc);
+        PlayerPrefs.SetString("Language", loc);
+    };
+
+    private void Start()
+    {
+        ChangeLocalization.Invoke(PlayerPrefs.GetString("Language", "ENG"));
+    }
+    
+    public void SetLocalization(string loc)
+    {
+        ChangeLocalization.Invoke(loc);
+    }
+    
+    private static void LoadLocalizationFile(string loc)
+    {
+        LocalizedText = new Dictionary<string, string>();
+        var name = "Localization_" + loc;
+        var asset = Resources.Load<TextAsset>(name);
+        var loadedData = JsonUtility.FromJson<LocalizationData>(asset.ToString());
+        foreach (var localizationItem in loadedData.items)
+        {
+            LocalizedText.Add(localizationItem.key, localizationItem.value);
+        }
+    }
+}
